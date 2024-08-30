19b686a3e72cb79f0988ab6d5c4ec3d8e81d4fd7
AadhithyaVenkatesan <aadhithyavenkatesan1@gmail.com>
2024-08-30 12:53:39 +0530

    Finished

--- lib/src/menu.dart
+++ lib/src/menu.dart
@@ -1,14 +1,49 @@
+import 'dart:async';
+import 'dart:io';   
+import 'package:path_provider/path_provider.dart';  
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+
+const String kExamplePage = '''
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Load file or HTML string example</title>
+</head>
+<body>
+
+<h1>Local demo page</h1>
+<p>
+ This is an example page used to demonstrate how to load a local file or HTML
+ string using the <a href="https://pub.dev/packages/webview_flutter">Flutter
+ webview</a> plugin.
+</p>
+
+</body>
+</html>
+''';
+
+
+
 
 enum _MenuOptions {
   navigationDelegate,
   userAgent,
   javascriptChannel,
+  listCookies,
+  clearCookies,
+  addCookie,
+  setCookie,
+  removeCookie,
+  loadFlutterAsset,
+  loadLocalFile,
+  loadHtmlString
 }
 
 class Menu extends StatefulWidget {
   const Menu({ required this.controller, super.key});
+
+
 
   final WebViewController controller;
 
@@ -17,6 +52,91 @@ class Menu extends StatefulWidget {
 }
 
 class _MenuState extends State<Menu> {
+
+  Future<void> _onLoadLocalFileExample(
+    WebViewController controller, BuildContext context) async {
+  final String pathToIndex = await _prepareLocalFile();
+
+  await controller.loadFile(pathToIndex);
+}
+
+static Future<String> _prepareLocalFile() async {
+  final String tmpDir = (await getTemporaryDirectory()).path;
+  final File indexFile = File('$tmpDir/www/index.html');
+
+  await Directory('$tmpDir/www').create(recursive: true);
+  await indexFile.writeAsString(kExamplePage);
+
+  return indexFile.path;
+}
+
+Future<void> _onLoadHtmlStringExample(
+    WebViewController controller, BuildContext context) async {
+  await controller.loadHtmlString(kExamplePage);
+}
+
+
+Future<void> _onLoadFlutterAssetExample(
+    WebViewController controller, BuildContext context) async {
+  await controller.loadFlutterAsset('assets/www/index.html');
+}
+
+  Future<void> _onListCookies(WebViewController controller)async{
+    final String cookies = await controller
+    .runJavaScriptReturningResult('document.cookie') as String;
+
+    if (!mounted) {
+      return;
+    }
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(cookies.isNotEmpty ? cookies : 'There are no cookies.')));
+  }
+
+  Future<void> _onClearCookies() async {
+  final hadCookies = await cookieManager.clearCookies();
+  String message = 'There were cookies. Now, they are gone!';
+  if (!hadCookies) {
+    message = 'There were no cookies to clear.';
+  }
+  if (!mounted) return;
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(message),
+    ),
+  );
+}
+
+  Future<void> _onAddCookie(WebViewController controller)async{
+    await controller.runJavaScript('''var date = new Date();
+  date.setTime(date.getTime()+(30*24*60*60*1000));
+  document.cookie = "FirstName=John; expires=" + date.toGMTString();''');
+  if(!mounted) return;
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Custom cookie added.')));
+  }
+
+
+
+  Future<void> _onSetCookie(WebViewController controller) async {
+    await cookieManager.setCookie(
+      const WebViewCookie(name: 'foo', value: 'bar',domain: 'flutter.dev')
+    );
+
+    if(!mounted) return;
+    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Custom cookie is set.')));
+  }
+
+  Future<void> _onRemoveCookie(WebViewController controller) async {
+    await controller.runJavaScript(
+      'document.cookie="FirstName=John; expires=Thu, 01 Jan 1970 00:00:00 UTC" '
+    );
+    if(!mounted) return;
+     ScaffoldMessenger.of(context).showSnackBar(
+    const SnackBar(
+      content: Text('Custom cookie removed.'),
+    ),
+  );
+  }
+
+  final cookieManager = WebViewCookieManager();
   @override
   Widget build(BuildContext context) {
     return PopupMenuButton<_MenuOptions>(
@@ -42,6 +162,25 @@ req.onload = function(){
 }''');
             
           
+          case _MenuOptions.clearCookies:                        
+            await _onClearCookies();
+          case _MenuOptions.listCookies:
+            await _onListCookies(widget.controller);
+          case _MenuOptions.addCookie:
+            await _onAddCookie(widget.controller);
+          case _MenuOptions.setCookie:
+            await _onSetCookie(widget.controller);
+          case _MenuOptions.removeCookie:
+            await _onRemoveCookie(widget.controller);
+           case _MenuOptions.loadFlutterAsset:             
+            if (!mounted) return;
+            await _onLoadFlutterAssetExample(widget.controller, context);
+          case _MenuOptions.loadLocalFile:
+            if (!mounted) return;
+            await _onLoadLocalFileExample(widget.controller, context);
+          case _MenuOptions.loadHtmlString:
+            if (!mounted) return;
+            await _onLoadHtmlStringExample(widget.controller, context);
         }
       },
       itemBuilder: (context) => [
@@ -56,6 +195,38 @@ req.onload = function(){
           value: _MenuOptions.javascriptChannel,
           child: Text('Lookup IP Address'),
         ), 
+        const PopupMenuItem<_MenuOptions>(                
+          value: _MenuOptions.clearCookies,
+          child: Text('Clear Cookies'),
+        ), 
+        const PopupMenuItem<_MenuOptions>(                
+          value: _MenuOptions.listCookies,
+          child: Text('List Cookies' ),
+        ), 
+        const PopupMenuItem<_MenuOptions>(                
+          value: _MenuOptions.addCookie,
+          child: Text('Add cookie'),
+        ), 
+        const PopupMenuItem<_MenuOptions>(                
+          value: _MenuOptions.setCookie,
+          child: Text('Set Cookie'),
+        ), 
+        const PopupMenuItem<_MenuOptions>(
+          value: _MenuOptions.removeCookie,
+          child: Text('Remove cookie'),
+        ),  
+         const PopupMenuItem<_MenuOptions>(                // Add from here
+          value: _MenuOptions.loadFlutterAsset,
+          child: Text('Load Flutter Asset'),
+        ),
+        const PopupMenuItem<_MenuOptions>(
+          value: _MenuOptions.loadHtmlString,
+          child: Text('Load HTML string'),
+        ),
+        const PopupMenuItem<_MenuOptions>(
+          value: _MenuOptions.loadLocalFile,
+          child: Text('Load local file'),
+        ),                                
       ],
     );
   }
